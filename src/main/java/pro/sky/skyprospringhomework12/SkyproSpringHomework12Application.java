ca98a23b36d5984871af8da9c4044e1568b94753
NataliAlanDiana <87333194+NataliAlanDiana@users.noreply.github.com>
2022-08-25 15:53:32 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringhomework12/SkyproSpringHomework12Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringhomework12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHomework12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHomework12Application.class, args);
+	}
+
+}
