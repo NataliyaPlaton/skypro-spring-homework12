cf617ac13677b8551adc3229afed61b634a81921
Nataliya <liya15rus@yandex.ru>
2022-09-15 02:05:58 +0300

    задание выполнено

--- src/main/java/pro/sky/skyprospringhomework12/service/CalculatorService.java
+++ src/main/java/pro/sky/skyprospringhomework12/service/CalculatorService.java
@@ -1,9 +1,11 @@
 package pro.sky.skyprospringhomework12.service;
 
 import org.springframework.stereotype.Service;
+import pro.sky.skyprospringhomework12.exception.DivisionByZeroException;
 
 @Service
 public class CalculatorService {
+
 
     public Number sum(Number num1, Number num2) {
         return num1.doubleValue() + num2.doubleValue();
@@ -18,6 +20,14 @@ public class CalculatorService {
     }
 
     public Number divide(Number num1, Number num2) {
+        if (Double.compare(num2.doubleValue(), 0) == 0) {
+            throw new DivisionByZeroException();
+        }
         return num1.doubleValue() / num2.doubleValue();
     }
+
+
 }
+
+
+
